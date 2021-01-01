bd98cc28c2221a72a330d7ce0a8fccea58c2581b
Arturo Casal <arturo.casal@imagames.com>
2021-01-01 23:07:54 +0100

    Fixed MQTT client not receiving commands after reconnect. Improved MQTT Termination procedure.

--- src/main/scala/net/bfgnet/cam2mqtt/mqtt/MqttSystem.scala
+++ src/main/scala/net/bfgnet/cam2mqtt/mqtt/MqttSystem.scala
@@ -5,10 +5,10 @@ import akka.actor.ActorRef
 import akka.actor.typed.scaladsl.Behaviors
 import akka.actor.typed.scaladsl.adapter._
 import akka.actor.typed.{Behavior, SupervisorStrategy}
-import akka.stream.alpakka.mqtt.scaladsl.MqttFlow
+import akka.stream.alpakka.mqtt.scaladsl.{MqttFlow, MqttMessageWithAck}
 import akka.stream.alpakka.mqtt.{MqttConnectionSettings, MqttMessage, MqttQoS, MqttSubscriptions}
-import akka.stream.scaladsl.{Flow, Keep, Sink, Source, SourceQueueWithComplete}
-import akka.stream.{ActorAttributes, OverflowStrategy, Supervision}
+import akka.stream.scaladsl.{Flow, Keep, RunnableGraph, Sink, Source}
+import akka.stream.{ActorAttributes, CompletionStrategy, OverflowStrategy, Supervision}
 import javax.net.ssl.SSLContext
 import net.bfgnet.cam2mqtt.camera.CameraProtocol.{CameraAvailableEvent, CameraEvent}
 import net.bfgnet.cam2mqtt.camera.modules.CameraModules
@@ -18,8 +18,8 @@ import net.bfgnet.cam2mqtt.mqtt.MqttProtocol._
 import net.bfgnet.cam2mqtt.utils.ActorContextImplicits
 import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence
 
-import scala.concurrent.Future
 import scala.concurrent.duration._
+import scala.concurrent.{ExecutionContext, Future}
 import scala.util.Try
 
 case class MqttBaseTopicProvider(base: String)
@@ -28,15 +28,15 @@ object MqttProtocol {
 
     sealed trait MqttCmd
 
-    case class CameraEventReceived(ev: CameraEvent) extends MqttCmd
+    case class CameraEventReceived(ev: CameraEvent, ack: Option[() => Future[Done]]) extends MqttCmd
 
     case object MQTTConnected extends MqttCmd
-
-    case class MQTTDisconnected(reason: Throwable) extends MqttCmd
 
     case object StreamCompleteInmediate extends MqttCmd
 
     case class TerminatedWithError(err: Throwable) extends MqttCmd
+
+    case class CamFinished(id: String) extends MqttCmd
 
     case object TerminatedOk extends MqttCmd
 
@@ -54,21 +54,18 @@ object MqttSystem extends ActorContextImplicits {
                 context.setLoggerName(MqttSystem.getClass)
                 context.log.info(s"starting MQTT client...")
 
-                val act = context.messageAdapter[CameraEvent](ev => CameraEventReceived(ev))
+                val act = context.messageAdapter[CameraEvent](ev => CameraEventReceived(ev, None))
 
                 val src = mqttStreamSource(baseTopic, connectionSettings(cfg))
-                val ((mqttStream, subscribed), result) = src.run()
-                subscribed.map { _ =>
-                    context.self ! MQTTConnected
-                }.recover {
+                val ((mqttStream, subscribed), sinkResult) = src.run()
+                // add mqtt stream event handlers
+                sinkResult.recover {
                     case e: Throwable =>
-                        context.self ! MQTTDisconnected(e)
+                        context.self ! TerminatedWithError(e)
                         throw e
                 }
-                mqttStream.watchCompletion().map { _ =>
-                    context.self ! TerminatedOk
-                }.recover {
-                    case e => context.self ! TerminatedWithError(e)
+                subscribed.map { _ =>
+                    context.self ! MQTTConnected
                 }
 
                 Behaviors.receiveMessagePartial[MqttCmd] {
@@ -78,84 +75,66 @@ object MqttSystem extends ActorContextImplicits {
                         CameraEventBus.bus.subscribe(act.toClassic, CameraEventBus.TOPIC_AVAILABILITY)
                         CameraEventBus.bus.subscribe(act.toClassic, CameraEventBus.TOPIC_OTHER)
                         connected(act.toClassic, mqttStream)
-                    case MQTTDisconnected(err) =>
+                    case TerminatedWithError(err) =>
                         context.log.error("mqtt client connection error", err)
-                        mqttStream.fail(err)
-                        CameraEventBus.bus.unsubscribe(act.toClassic)
-                        failing()
+                        // stream already failed. just force restart through supervisor
+                        throw err
                     case Terminate =>
-                        mqttStream.complete()
+                        mqttStream ! Terminate
                         terminatingOk()
                 }
             }
         }.onFailure(SupervisorStrategy.restartWithBackoff(minBackoff = 4.seconds, maxBackoff = 1.minute, randomFactor = 0.2))
     }
 
-    def mqttStreamSource(baseTopic: String, settings: MqttConnectionSettings) = {
-        val inputTopic = s"$baseTopic/camera/+/command/#"
-
-        val mqttFlow: Flow[MqttMessage, MqttMessage, Future[Done]] =
-            MqttFlow.atMostOnce(
-                settings,
-                MqttSubscriptions(inputTopic, MqttQoS.AtLeastOnce),
-                bufferSize = 8,
-                MqttQoS.AtLeastOnce
-            )
-        val alwaysStop: Supervision.Decider = (_ => Supervision.Stop)
-        val stratAlwaysStop = ActorAttributes.supervisionStrategy(alwaysStop)
-
-        implicit val baseTopicProv = MqttBaseTopicProvider(baseTopic)
-
-        Source
-                .queue(200, OverflowStrategy.fail)
-                .map { camEv =>
-                    event2Mqtt(camEv)
-                }
-                .filter(_.isDefined)
-                .map(_.get)
-                .viaMat(mqttFlow)(Keep.both)
-                .map { ev =>
-                    Try {
-                        MqttCommands.inputCommand(baseTopic, ev)
-                    }
-                }
-                .toMat(Sink.ignore)(Keep.both)
-                .withAttributes(stratAlwaysStop)
-    }
-
-    private def connected(eventBusListener: ActorRef, mqttStream: SourceQueueWithComplete[CameraEvent]): Behavior[MqttCmd] = {
+    private def connected(eventBusListener: ActorRef, mqttStream: ActorRef): Behavior[MqttCmd] = {
         Behaviors.setup { implicit context =>
             Behaviors.receiveMessagePartial {
-                case CameraEventReceived(ev) =>
-                    mqttStream.offer(ev)
+                case ev: CameraEventReceived =>
+                    mqttStream ! ev
                     Behaviors.same
-                case MQTTConnected =>
-                    context.log.info("mqtt client successfully connected")
-                    CameraEventBus.bus.subscribe(eventBusListener, CameraEventBus.TOPIC_MOTION)
-                    CameraEventBus.bus.subscribe(eventBusListener, CameraEventBus.TOPIC_AVAILABILITY)
-                    connected(eventBusListener, mqttStream)
-                case MQTTDisconnected(err) =>
+                case TerminatedWithError(err) =>
                     context.log.error("mqtt client connection error", err)
-                    mqttStream.fail(err)
                     CameraEventBus.bus.unsubscribe(eventBusListener)
-                    failing()
+                    // stream already failed. just force restart through supervisor
+                    throw err
                 case Terminate =>
                     CameraEventBus.bus.unsubscribe(eventBusListener)
-                    //mqttStream.complete()
                     sendingOffline(mqttStream)
+                case TerminatedOk =>
+                    Behaviors.same
             }
         }
     }
 
-    private def sendingOffline(mqttStream: SourceQueueWithComplete[CameraEvent]): Behavior[MqttCmd] =
+    private def sendingOffline(mqttStream: ActorRef): Behavior[MqttCmd] =
         Behaviors.setup { context =>
-            context.scheduleOnce(1.seconds, context.self, TerminatedOk)
-            ConfigManager.cameras.map(_.cameraId).foreach { id =>
-                mqttStream.offer(CameraAvailableEvent(id, available = false))
+            implicit val _ec: ExecutionContext = context.executionContext
+            context.scheduleOnce(5.seconds, context.self, TerminatedOk)
+            val camIds = ConfigManager.cameras.map(_.cameraId)
+            camIds.foreach { id =>
+                mqttStream ! CameraEventReceived(CameraAvailableEvent(id, available = false), Option(() => Future {
+                    context.self ! CamFinished(id)
+                    Done
+                }))
             }
+            sendingOffline(mqttStream, camIds)
+        }
+
+    private def sendingOffline(mqttStream: ActorRef, remaining: List[String]): Behavior[MqttCmd] =
+        Behaviors.setup { _ctxt =>
             Behaviors.receiveMessagePartial {
+                case CamFinished(id) if remaining.contains(id) =>
+                    val rem = remaining.filterNot(_ == id)
+                    if (rem.nonEmpty) {
+                        sendingOffline(mqttStream, rem)
+                    } else {
+                        mqttStream ! Terminate
+                        _ctxt.self ! TerminatedOk
+                        terminatingOk()
+                    }
                 case TerminatedOk =>
-                    mqttStream.complete()
+                    mqttStream ! Terminate
                     terminatingOk()
                 case TerminatedWithError(_) =>
                     Behaviors.stopped
@@ -174,22 +153,59 @@ object MqttSystem extends ActorContextImplicits {
                 Behaviors.same
         }
 
-    private def failing(): Behavior[MqttCmd] =
-        Behaviors.receiveMessagePartial {
-            case TerminatedOk =>
-                throw new Exception("stream failed")
-            case TerminatedWithError(err) =>
-                throw err
-            case _ =>
-                Behaviors.same
+    def mqttStreamSource(baseTopic: String, settings: MqttConnectionSettings): RunnableGraph[((ActorRef, Future[Done]), Future[Done])] = {
+        val inputTopic = s"$baseTopic/camera/+/command/#"
+
+        val mqttFlow: Flow[MqttMessageWithAck, MqttMessageWithAck, Future[Done]] =
+            MqttFlow.atLeastOnceWithAck(
+                settings,
+                MqttSubscriptions(inputTopic, MqttQoS.AtLeastOnce),
+                bufferSize = 8,
+                MqttQoS.AtLeastOnce
+            )
+        val alwaysStop: Supervision.Decider = _ => Supervision.Stop
+        val stratAlwaysStop = ActorAttributes.supervisionStrategy(alwaysStop)
+
+        implicit val baseTopicProv: MqttBaseTopicProvider = MqttBaseTopicProvider(baseTopic)
+
+        val compStage: PartialFunction[Any, CompletionStrategy] = {
+            case Terminate => CompletionStrategy.immediately
         }
+        val failMatcher: PartialFunction[Any, Throwable] = {
+            case Terminate => new Exception("termination request")
+        }
+
+        Source
+                .actorRef[CameraEventReceived](compStage, failMatcher, 200, OverflowStrategy.dropTail)
+                .map { camEv =>
+                    event2Mqtt(camEv.ev) match {
+                        case Some(e) if camEv.ack.isDefined =>
+                            Some(e.withAck(camEv.ack.get))
+                        case Some(e) =>
+                            Some(e.withoutAck())
+                        case _ => None
+                    }
+                }
+                .filter(_.isDefined)
+                .map(_.get)
+                .viaMat(mqttFlow)(Keep.both)
+                .map { ev =>
+                    Try {
+                        MqttCommands.inputCommand(baseTopic, ev.message)
+                    }
+                }
+                .toMat(Sink.ignore)(Keep.both)
+                .withAttributes(stratAlwaysStop)
+    }
 
     private def connectionSettings(cfg: MqttConfig): MqttConnectionSettings = {
         Option(MqttConnectionSettings(
             s"${if (cfg.ssl) "ssl" else "tcp"}://${cfg.host}:${cfg.port}",
-            "cam2mqtt-client",
+            s"cam2mqtt-client-${System.currentTimeMillis()}",
             new MemoryPersistence()
-        ).withAuth(cfg.username, cfg.password).withAutomaticReconnect(true).withCleanSession(true)).map { v =>
+        ).withAuth(cfg.username, cfg.password)
+                .withAutomaticReconnect(false)
+                .withCleanSession(false)).map { v =>
             if (cfg.ssl) v.withSocketFactory(SSLContext.getDefault.getSocketFactory) else v
         }.get
     }
@@ -201,4 +217,23 @@ object MqttSystem extends ActorContextImplicits {
 
     private def moduleEvent2Mqtt(ev: CameraEvent)(implicit _btp: MqttBaseTopicProvider): Option[MqttMessage] =
         CameraModules.MQTT_MODULES.find(_.moduleId == ev.moduleId).flatMap(_.eventToMqttMessage(ev))
+
+    private implicit class MqttMessageAckExt(msg: MqttMessage) {
+        def withAck(f: () => Future[Done]): MqttMessageWithAck = {
+            new MqttMessageWithAck {
+                override val message: MqttMessage = msg
+
+                override def ack(): Future[Done] = f()
+            }
+        }
+
+        def withoutAck(): MqttMessageWithAck = {
+            new MqttMessageWithAck {
+                override val message: MqttMessage = msg
+
+                override def ack(): Future[Done] = Future.successful(Done)
+            }
+        }
+    }
+
 }
